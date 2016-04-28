9c365e1f870c4157d457c2062df47d992e173185
Shubham Jha <shubhamjha3003@gmail.com>
2016-04-28 19:34:39 +0530

    Scrolling Functionality Added
    Earlier, on screen rotation, we could not see all entries as number of
    apps was large. Thus, scrolling functionality was added.

--- app/src/main/java/shubhamjha33/myappportfolio/app/MainActivityFragment.java
+++ app/src/main/java/shubhamjha33/myappportfolio/app/MainActivityFragment.java
@@ -5,6 +5,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,7 +22,18 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView=inflater.inflate(R.layout.fragment_main, container, false);
+        List<String> appList=new ArrayList();
+        appList.add("Spotify Streamer");
+        appList.add("Football Scores");
+        appList.add("Library");
+        appList.add("Build It Bigger");
+        appList.add("XYZ Reader");
+        appList.add("Capstone");
+        ArrayAdapter<String> arrayAdapter=new ArrayAdapter<String>(getContext(),R.layout.list_item_button,R.id.launcherButton,appList);
+        ListView buttonList=(ListView)rootView.findViewById(R.id.listView);
+        buttonList.setAdapter(arrayAdapter);
+        return rootView;
     }
 
 
